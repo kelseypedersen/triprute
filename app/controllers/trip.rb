6215b0737e8693c618ab3f5452d58d8c8d1a1a8a
Kelsey Pedersen <kelseypedersen@gmail.com>
2015-05-23 13:25:22 -0700

    Move destination routes from trip controller to destination controller

--- app/controllers/trip.rb
+++ app/controllers/trip.rb
@@ -24,26 +24,6 @@ end
 # Redirects to create a new destination page
 
 
-# Return an HTML form for creating a new destination
-get '/trips/:id/destinations/new' do
-  @trip = Trip.find(params[:id])
-  "This is the new destinations page!"
-  erb :'destinations/new'
-end
-
-post '/trips/:id/destinations' do
-  p params
-  @trip = Trip.find(params[:id])
-  @destination = Destination.create(city: params[:city], country: params[:country], highlights: params[:highlights], lodging: params[:lodging], transportation: params[:transportation], days: params[:days], trip_id: params[:id])
-  redirect "/trips/#{@trip.id}/destinations/#{@destination.id}"
-end
-
-get '/trips/:id/destinations/:destination_id' do
-  @trip = Trip.find(params[:id])
-  @destination = Destination.find(params[:destination_id])
-  erb :'trips/show'
-end
-
 
 # post '/trips/:id' do
 #   #@trip = Trip.find(params[:id])
